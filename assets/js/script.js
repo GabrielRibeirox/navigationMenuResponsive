1ad4d4ba42429352bc79d9050079c658bd46ffa7
Gabriel Ribeiro <gabriellrsilva8@gmail.com>
2021-06-09 09:57:12 -0300

    first commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,20 @@
-console.log('Olá ');
 
+  const buttonMenu = document.querySelector('.button-menu');
+  let menu = document.querySelector('.container-menu')
+
+
+  function menuActive(){
+      
+
+
+  }
+  buttonMenu.addEventListener('click',function(event){ 
+    if(menu.classList.contains("active")){
+      menu.classList.remove('active');
+      menu.classList.add('disable');
+    }else{
+      menu.classList.remove('disable');
+      menu.classList.add('active');
+    }
+});
 
